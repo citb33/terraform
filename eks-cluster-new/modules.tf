49b477ad3cc9c24a4560254b104bdb2f3c0d288c
Raghu K <raghuk.vit@gmail.com>
2019-06-09 19:55:09 +0530

    --- Auto-Git Commit ---

--- eks-cluster-new/modules.tf
+++ eks-cluster-new/modules.tf
@@ -1,2 +1,13 @@
 // Create Route 53 hosted zone
 
+module "blueharvest-eks" {
+  source              = "armandorvila/blueharvest-eks/aws"
+  availability_zones  = ["us-east-2a", "us-east-2b", "us-east-2c"]
+  eks_ami_id          = "ami-04ea7cb66af82ae4a" //amazon-eks-node-1.11-v20190109
+  instance_type       = "t2.micro"
+  asg_min_size        = "1"
+  asg_max_size        = "2"
+  cluster_name        = "my_cluster"
+  cluster_zone        = "devopsproj.io"
+  cluster_zone_id     = "Z3UN578ZESLH2O"
+}
