f7f42a459a43abd14411ca2a5bd534c9fcb1cb7f
Raghu K <raghuk.vit@gmail.com>
2019-06-09 20:46:10 +0530

    --- Auto-Git Commit ---

--- eks-cluster-new/modules.tf
+++ eks-cluster-new/modules.tf
@@ -3,7 +3,7 @@
 module "eks" {
   source                = "terraform-aws-modules/eks/aws"
   cluster_name          = "test-eks-cluster"
-  subnets               = ["subnet-abcde012", "subnet-bcde012a"]
+  subnets               = ["subnet-dcf4e1b5", "subnet-bcde012a"]
   tags                  = "${map("Environment", "test")}"
   vpc_id                = "vpc-4271752b"
 }
