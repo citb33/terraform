8c017c872665ccb63b4067bf754bb086f2ea81df
Raghu K <raghuk.vit@gmail.com>
2019-06-09 19:55:19 +0530

    --- Auto-Git Commit ---

--- eks-cluster-new/modules.tf
+++ eks-cluster-new/modules.tf
@@ -3,7 +3,7 @@
 module "blueharvest-eks" {
   source              = "armandorvila/blueharvest-eks/aws"
   availability_zones  = ["us-east-2a", "us-east-2b", "us-east-2c"]
-  eks_ami_id          = "ami-04ea7cb66af82ae4a" //amazon-eks-node-1.11-v20190109
+  eks_ami_id          = "ami-04ea7cb66af82ae4a" 
   instance_type       = "t2.micro"
   asg_min_size        = "1"
   asg_max_size        = "2"
